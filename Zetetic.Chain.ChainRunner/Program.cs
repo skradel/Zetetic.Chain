4332fbf21ff7df2773a20300def1a20cab2051b6
unknown <Administrator@.sk5t.net>
2010-02-04 14:21:58 -0500

    Added BatchCommand for running processes to core library. Added CLI application for running a command.

---
+++ Zetetic.Chain.ChainRunner/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zetetic.Chain;
+
+namespace Zetetic.Chain
+{
+    public class ChainRunner
+    {
+        static void Main(string[] args)
+        {
+            string catalogsrc = "catalog.xml", command = "default";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-c":
+                        catalogsrc = args[++i];
+                        break;
+
+                    case "-x":
+                        command = args[++i];
+                        break;
+                }
+            }
+
+            try
+            {
+                ICatalog catalog = CatalogFactory.GetFactory().GetCatalog(catalogsrc);
+                IContext ctx = new ContextBase();
+
+                catalog[command].Execute(ctx);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message + " :: " + ex.StackTrace);
+                Environment.ExitCode = 1;
+            }
+        }
+    }
+}
---
+++ src/Commands/BatchCommand.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zetetic.Chain;
+using System.Diagnostics;
+using NLog;
+
+namespace Zetetic.Chain.Commands
+{
+    public class BatchCommand : Command
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        [ChainRequired]
+        public string Image { get; set; }
+        public string Arguments { get; set; }
+        public string WorkingDirectory { get; set; }
+        public bool UseShell { get; set; }
+        public bool ThrowError { get; set; }
+        public bool KillAfterWait { get; set; }
+        public int MaxWaitTime { get; set; }
+
+        protected virtual ProcessStartInfo CreateStartInfo()
+        {
+            ProcessStartInfo pi = new ProcessStartInfo(this.Image, this.Arguments);
+            pi.CreateNoWindow = true;
+            pi.WindowStyle = ProcessWindowStyle.Hidden;
+
+            if (!string.IsNullOrEmpty(this.WorkingDirectory))
+                pi.WorkingDirectory = this.WorkingDirectory;
+
+            pi.UseShellExecute = this.UseShell;
+
+            logger.Debug("Proc image {0}, args {1}, wd {2}, shell {3}",
+                this.Image, this.Arguments, pi.WorkingDirectory, pi.UseShellExecute);
+
+            return pi;
+        }
+
+        public override CommandResult Execute(IContext ctx)
+        {
+            ProcessStartInfo pi = this.CreateStartInfo();
+
+            using (Process p = new Process())
+            {
+                p.StartInfo = pi;
+
+                bool started = p.Start();
+                logger.Info("Started {0}: {1}", pi.FileName, started);
+
+                int wtim = 30;
+                long waited = 0;
+
+                while (!p.HasExited)
+                {
+                    System.Threading.Thread.Sleep(wtim);
+
+                    if ((waited += wtim) > this.MaxWaitTime && this.MaxWaitTime > 0)
+                    {
+                        if (this.KillAfterWait)
+                            p.Kill();
+
+                        throw new ApplicationException("Process exceeded the maximum wait time of "
+                            + this.MaxWaitTime + " ms");
+                    }
+                }
+
+                int rc = p.ExitCode;
+
+                logger.Info("Exit code {0}", rc);
+
+                ctx["LastExitCode"] = p.ExitCode;
+
+                if (rc != 0 && this.ThrowError)
+                    throw new ApplicationException("Nonzero return code");
+            }
+
+            return CommandResult.Continue;
+        }
+    }
+}
